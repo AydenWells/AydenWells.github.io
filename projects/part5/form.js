540e4d79e634aedfc143afc559834cc5918b2dc1
AydenWells.github.io <you@example.com>
2023-10-29 20:16:11 -0400

    added assignment 11

--- projects/part5/form.js
+++ projects/part5/form.js
@@ -1,19 +1,19 @@
 const submitProduct = (e) => {
-    e.preventDefault();
-    document.getElementById("results").classList.remove("hidden");
-  
-    const form = document.getElementById("form-product");
-    const productName = form.elements["product-name"].value;
-    const colors = form.elements["colors"].value;
-    const sizes = form.elements["sizes"].value;
-    const termsChecked = form.elements["terms"].checked;
-  
-    console.log(raccName);
-    console.log(colors);
-    console.log(sizes);
-    console.log(termsChecked);
-  };
-  
-  
-  
-  document.getElementById("form-product").onsubmit = submitProduct;
+  e.preventDefault();
+  const form = document.getElementById("form-product");
+  const productName = form.elements["product-name"].value;
+  const colors = form.elements["colors"].value;
+  const sizes = form.elements["sizes"].value;
+  const termsChecked = form.elements["terms"].checked;
+
+  const message = document.createElement("p");
+  message.innerHTML = `Product Name: ${productName}<br>Main Color Presented: ${colors}<br>Main Size Presented: ${sizes}<br>Terms Agreed: ${termsChecked ? "Yes" : "No"}<br>Sent to Headquarters for Review!`;
+
+  const results = document.getElementById("results");
+  results.innerHTML = "";
+  results.appendChild(message);
+
+  results.classList.remove("hidden");
+};
+
+document.getElementById("form-product").onsubmit = submitProduct;
